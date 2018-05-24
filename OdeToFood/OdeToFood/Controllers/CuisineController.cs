c44d2a5b8a16957539bdccf834d716b9a28cb239
Joe <josephbarkate@gmail.com>
2018-05-24 13:50:17 -0500

    Adding project to repo

---
+++ OdeToFood/OdeToFood/Controllers/CuisineController.cs
@@ -0,0 +1,25 @@
+﻿using OdeToFood.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    [Log]
+    public class CuisineController : Controller
+    {
+        //
+        // GET: /Cuisine/
+        [Authorize]
+        public ActionResult Search(string name)
+        {
+            var message = Server.HtmlEncode(name);
+            return Content(name);
+           // return RedirectToAction("Index", "Home", new { name = name });
+        }
+
+
+    }
+}
